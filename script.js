8864d71808cfb7ef96405d12564d8ea3745f5e0b
melchior2137 <84140494+melchior2137@users.noreply.github.com>
2021-06-02 12:36:39 +0200

    add animations

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let itemIndex = 1; // default Player item
+let bodyElement = document.querySelector("body");
+let computerScore=0;
+let playerScore=0;
 items = ["ROCK", "PAPER", "SCISSORS"]
 
 //ZROBIC TABLICE OBRAZKOW
@@ -12,49 +15,98 @@ function nextItem() {
     if (itemIndex > 2) {
         itemIndex = 0;
     }
-    document.getElementById("playerItem").innerHTML = items[itemIndex];
+    document.getElementById("playerItem").animate([
+        // keyframes
+        { transform: 'rotate(360deg)' },
+      ], {
+        // timing options
+        duration: 400,
+        iterations: 1
+      });
+      setTimeout(function(){
+      document.getElementById("playerItem").innerHTML = items[itemIndex];
+      },300)
 }
 function prevItem() {
     itemIndex--;
     if (itemIndex < 0) {
         itemIndex = 2;
     }
-    document.getElementById("playerItem").innerHTML = items[itemIndex];
+    document.getElementById("playerItem").animate([
+        // keyframes
+        { transform: 'rotate(-360deg)' },
+      ], {
+        // timing options
+        duration: 400,
+        iterations: 1
+      });
+      setTimeout(function(){
+      document.getElementById("playerItem").innerHTML = items[itemIndex];
+      },300)
 }
 function play(playerItem) {                     //main game function
     cpuIndex = cpuIndexGen(0, 3);
-    document.getElementById("cpuItem").innerHTML = items[cpuIndex];
+
+    document.getElementById("cpuItem").animate([
+        // keyframes
+        { transform: 'rotate(360deg)' },
+      ], {
+        // timing options
+        duration: 400,
+        iterations: 1
+      });
+      setTimeout(function(){
+      document.getElementById("cpuItem").innerHTML = items[cpuIndex];
+      },300)
+
+
+    
     setTimeout(function(){
         if (cpuIndex == playerItem) {
-        alert("DRAW");
+        bodyElement.style.border = "25px solid #F4DD0F";
+        playerScore++;
+        computerScore++;
     }
     else{
         switch(cpuIndex){
             case 0:
                 if(playerItem==1){
-                    alert("WIN");
+                    bodyElement.style.border = "25px solid #44E179";
+                    playerScore++;
                 }
                 else{
-                    alert("LOSE");
+                    bodyElement.style.border = "25px solid #E14444";
+                    computerScore++;
+
                 }
                 break;
             case 1:
                 if(playerItem==2){
-                    alert("WIN");
+                    bodyElement.style.border = "25px solid #44E179";
+                    playerScore++;
+
                 }
                 else{
-                    alert("LOSE");
+                    bodyElement.style.border = "25px solid #E14444";
+                    computerScore++;
+
                 }
                 break;
             case 2:
                 if(playerItem==0){
-                    alert("WIN");
+                    bodyElement.style.border = "25px solid #44E179";
+                    playerScore++;
+
                 }
                 else{
-                    alert("LOSE");
+                    bodyElement.style.border = "25px solid #E14444";
+                    computerScore++;
                 }
                 break;
         }
     }
-},35)
+    document.getElementById("score").innerHTML = playerScore + " : " + computerScore;
+},500)
 };
+
+
